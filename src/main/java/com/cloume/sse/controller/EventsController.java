1c3fb6d2d9c89333b66041de22aa2cd2ef7a5dc7
SUGENAN <gxu@cloume.com>
2024-02-05 19:11:28 +0800

    增加测试文件；rename

--- src/main/java/com/cloume/sse/controller/EventsController.java
+++ src/main/java/com/cloume/sse/controller/EventsController.java
@@ -1,2 +1,68 @@
-package com.cloume.sse.controller;public class EventsController {
+package com.cloume.sse.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+@RestController
+@RequestMapping("/events")
+public class EventsController {
+    private final ConcurrentHashMap<String, Consumer<String>> listeners = new ConcurrentHashMap<>();
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> streamEvents() {
+        return Flux.create(emitter -> {
+            Consumer<String> listener = emitter::next;
+            String id = registerListener(listener);
+            emitter.onDispose(() -> unregisterListener(id));
+        }, FluxSink.OverflowStrategy.BUFFER);
+    }
+
+    private String registerListener(Consumer<String> listener) {
+        String id = generateUniqueId();
+        listeners.put(id, listener);
+        return id;
+    }
+
+    private void unregisterListener(String id) {
+        listeners.remove(id);
+    }
+
+    private String generateUniqueId() {
+        // 实现生成唯一ID的逻辑
+        return String.valueOf(System.currentTimeMillis());
+    }
+
+    // 在类中添加一个ObjectMapper实例
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private void sendMessageToAllListeners(String target, String event) {
+        Map<String, String> messageMap = new HashMap<>();
+        messageMap.put("target", target);
+        messageMap.put("event", event);
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(messageMap);
+            listeners.values().forEach(listener -> listener.accept(jsonMessage));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostMapping
+    public Mono<Map<String, String>> onQuery(@RequestBody Map<String, String> body) {
+
+        sendMessageToAllListeners(body.getOrDefault("text", "N/A"), "clicked");
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "OK");
+        return Mono.just(response);
+    }
 }
---
+++ src/test/java/com/cloume/sse/SpringSSEDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloume.sse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSSEDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
